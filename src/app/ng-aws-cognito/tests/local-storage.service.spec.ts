63f15cb73de4eda33826c9634e6deec3c031ccb6
Kevin Aud <kevinaud@gmail.com>
2017-07-12 19:33:26 -0500

    updated stuff

---
+++ src/app/ng-aws-cognito/tests/local-storage.service.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from "@angular/core/testing";
+
+import { LocalStorageService } from "../local-storage.service";
+
+describe("Service: LocalStorageService", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        LocalStorageService
+      ]
+    });
+  });
+
+  it("should ...", inject([ LocalStorageService ], (service: LocalStorageService) => {
+    expect(service).toBeTruthy();
+  }));
+});
+
